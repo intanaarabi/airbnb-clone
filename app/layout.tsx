66ff5f49326c0ad9acd2ef5e7a02d59b6f5676e6
Dayang Intan <intan.abangarabi@gmail.com>
2023-10-22 12:08:57 +0800

    add ClientOnly component for client-side rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Nunito } from "next/font/google"
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -21,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+          <Modal />  
+        </ClientOnly>
         {children}
       </body>
     </html>
