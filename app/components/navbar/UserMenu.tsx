7cae4e1d779897f68b3b6ec8e7d0558473fdfccd
Dayang Intan <intan.abangarabi@gmail.com>
2023-10-22 11:58:01 +0800

    Add menu items to menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,6 +3,7 @@
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -70,7 +71,14 @@ const UserMenu = () => {
             >
                 <div className='flex flex-col cursor-pointer'>
                     <>
-                    {/* <MenuItem/> */}
+                    <MenuItem
+                        onClick={() => {}}
+                        label="Login"
+                    />
+                    <MenuItem
+                        onClick={() => {}}
+                        label="Sign Up"
+                    />
                     </>
                 </div>
             </div>
